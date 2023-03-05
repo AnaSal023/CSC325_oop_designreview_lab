bff2118209ef59c96cc6f1e3936ba302f989c4ef
anasa <anasa@LAPTOP-FPAKTMJC>
2023-03-04 21:29:03 -0500

    fix the resulting errors

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
@@ -10,6 +10,16 @@ package com.mycompany.csc325_oop_designreview_lab;
  * @author MoaathAlrajab
  */
 public class Student extends Human{
+
+    @Override
+    public String getAddress() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void setAddress(String address) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 	// ToDo 1: Make this class a child of Human
 	
 	// ToDo 2: Fix the resulting errors
