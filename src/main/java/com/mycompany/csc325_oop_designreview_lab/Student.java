7498e444d126492e105a17703024d38ab0dfbd8f
anasa <anasa@LAPTOP-FPAKTMJC>
2023-03-04 21:37:10 -0500

    add comments to code

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
@@ -10,9 +10,9 @@ package com.mycompany.csc325_oop_designreview_lab;
  * @author MoaathAlrajab
  */
 public class Student extends Human{
-    
+    //GPA field
     private double gpa;
-
+    //implemented and overrride the asbtract methods of the abstract Human class
     @Override
     public String getAddress() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
@@ -27,6 +27,7 @@ public class Student extends Human{
 	// ToDo 2: Fix the resulting errors
 	
 	// ToDo 3: Add a field for GPA and create setter and getter
+    //created get and set methods of the GPA field
     public double getGpa() {
         return gpa;
     }
@@ -35,5 +36,5 @@ public class Student extends Human{
         this.gpa = gpa;
     }
 	// ToDo 4: Add comments to your code
-
+    
 }
