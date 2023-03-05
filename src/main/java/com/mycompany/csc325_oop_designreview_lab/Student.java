42d609ad5bc45ba34d05363cc7f07c14552d1af8
anasa <anasa@LAPTOP-FPAKTMJC>
2023-03-04 21:49:38 -0500

    Fix the constructor of Student class

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
@@ -12,6 +12,18 @@ package com.mycompany.csc325_oop_designreview_lab;
 public class Student extends Human{
     //GPA field
     private double gpa;
+    //credit field
+    private double credits;
+    //default constructor
+    public Student(String name, short age) {
+        super(name, age);
+    }
+
+    public Student(double credits, String name, short age) {
+        super(name, age);
+        this.credits = credits;
+    }
+    
     //implemented and overrride the asbtract methods of the abstract Human class
     @Override
     public String getAddress() {
@@ -36,5 +48,13 @@ public class Student extends Human{
         this.gpa = gpa;
     }
 	// ToDo 4: Add comments to your code
+    //created get and set methods for credits field
+    public double getCredits() {
+        return credits;
+    }
+
+    public void setCredits(double credits) {
+        this.credits = credits;
+    }
     
 }
